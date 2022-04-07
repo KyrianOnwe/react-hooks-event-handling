03e3ffdecc2bcae6f986ad080da0c5bc805099ca
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-07 01:07:56 -0500

    commit tree 9cfe5eef8f5d6735f69ed6a1a1ea22701b30595a

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -3,9 +3,13 @@ import React from "react";
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
-  }
+  } 
 
-  return <button>Tickle me!</button>;
+ /* return <button>Tickle me!</button>;
+ #The update is:  */
+ return <button onClick={tickle}>Tickle me!</button>
+ /*##or replace everything in second function like so: */
+ /*return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;*/
 }
 
 export default Tickler;
